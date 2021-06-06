888cd3ab052d1428cb9b5c3f6ae92196aa190813
Quyet <vanquyetx97@gmail.com>
2021-06-06 18:54:39 +0700

    first commit

---
+++ src/main/java/com/example/springbootcrud/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.springbootcrud.service;
+
+import io.github.tubean.myspringcrud.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> getAllUser();
+
+    void saveUser(User user);
+
+    void deleteUser(Long id);
+
+    Optional<User> findUserById(Long id);
+}
